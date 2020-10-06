cf871800b277110f17f3a683695a1c34dbf70276
charan <charan9487@gmail.com>
2020-10-06 19:15:04 +0530

    added filter feauture

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,18 @@ const Tour = require("../models/tourModel")
 
 exports.getAllTours = async (req, res) => {
             try {
-                        const tours = await Tour.find()
+                        const queryObj = { ...req.query };
+                        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+                        excludedFields.forEach(el => delete queryObj[el])
+
+                        //^Build query
+                        const query = Tour.find(queryObj);
+
+                        //*Execute query
+                        const tours = await query;
+
+
+
                         return res.status(200).json({
                                     status: "success",
                                     results: tours.length,
